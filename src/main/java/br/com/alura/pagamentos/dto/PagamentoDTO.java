4b4f5ab5d1158f45003328ee3c17acb0ff92713d
Adriel Renato <adriel.renato26@gmail.com>
2025-03-26 18:22:27 -0300

    Criando um controller para ter endpoint na api de pagamentos

--- src/main/java/br/com/alura/pagamentos/dto/PagamentoDTO.java
+++ src/main/java/br/com/alura/pagamentos/dto/PagamentoDTO.java
@@ -7,9 +7,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 public record PagamentoDTO(
         @NotNull
         @Positive
