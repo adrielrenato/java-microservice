0ac6bf4be190af2df19cdbb7711c808899ffe061
Adriel Renato <adriel.renato26@gmail.com>
2025-03-27 13:28:18 -0300

    Criando um feign client para fazer a comunicação entre os serviços

--- src/main/java/br/com/alura/pagamentos/service/PagamentoService.java
+++ src/main/java/br/com/alura/pagamentos/service/PagamentoService.java
@@ -1,6 +1,7 @@
 package br.com.alura.pagamentos.service;
 
 import br.com.alura.pagamentos.dto.PagamentoDTO;
+import br.com.alura.pagamentos.http.PedidoClient;
 import br.com.alura.pagamentos.model.Pagamento;
 import br.com.alura.pagamentos.model.Status;
 import br.com.alura.pagamentos.repository.PagamentoRepository;
@@ -11,6 +12,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PagamentoService {
     @Autowired
@@ -18,6 +21,9 @@ public class PagamentoService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private PedidoClient pedido;
 
     public Page<PagamentoDTO> obterTodos(Pageable paginacao) {
         return repository
@@ -49,5 +55,17 @@ public class PagamentoService {
 
     public void excluirPagamento(Long id) {
         repository.deleteById(id);
+    }
+
+    public void confirmarPagamento(Long id){
+        Optional<Pagamento> pagamento = repository.findById(id);
+
+        if (!pagamento.isPresent()) {
+            throw new EntityNotFoundException();
+        }
+
+        pagamento.get().setStatus(Status.CONFIRMADO);
+        repository.save(pagamento.get());
+        pedido.atualizaPagamento(pagamento.get().getPedidoId());
     }
 }
